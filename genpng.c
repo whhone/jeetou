23424338c144b6f141f6b089c29855ed38a2b057
Wai Hon Law <whhone@whhone-mbp.local>
2015-11-18 01:40:59 -0800

    format code with clang-format

--- genpng.c
+++ genpng.c
@@ -6,202 +6,189 @@
 #include <png.h>
 
 typedef struct image_s {
-    int width, height, stride;
-    unsigned char *buffer;      // RGB24
+  int width, height, stride;
+  unsigned char *buffer;  // RGB24
 } image_t;
 
 ASS_Library *ass_library;
 ASS_Renderer *ass_renderer;
 
-void msg_callback(int level, const char *fmt, va_list va, void *data)
-{
-    if (level > 6)
-        return;
-    printf("libass: ");
-    vprintf(fmt, va);
-    printf("\n");
+void msg_callback(int level, const char *fmt, va_list va, void *data) {
+  if (level > 6) return;
+  printf("libass: ");
+  vprintf(fmt, va);
+  printf("\n");
 }
 
-static void write_png(char *fname, image_t *img)
-{
-    FILE *fp;
-    png_structp png_ptr;
-    png_infop info_ptr;
-    png_byte **row_pointers;
-    int k;
+static void write_png(char *fname, image_t *img) {
+  FILE *fp;
+  png_structp png_ptr;
+  png_infop info_ptr;
+  png_byte **row_pointers;
+  int k;
 
-    png_ptr =
-        png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    info_ptr = png_create_info_struct(png_ptr);
-    fp = NULL;
+  png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  info_ptr = png_create_info_struct(png_ptr);
+  fp = NULL;
 
-    if (setjmp(png_jmpbuf(png_ptr))) {
-        png_destroy_write_struct(&png_ptr, &info_ptr);
-        fclose(fp);
-        return;
-    }
-
-    fp = fopen(fname, "wb");
-    if (fp == NULL) {
-        printf("PNG Error opening %s for writing!\n", fname);
-        return;
-    }
-
-    png_init_io(png_ptr, fp);
-    png_set_compression_level(png_ptr, 0);
-
-    png_set_IHDR(png_ptr, info_ptr, img->width, img->height,
-                 8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
-                 //8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
-                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-    png_write_info(png_ptr, info_ptr);
-
-    png_set_bgr(png_ptr);
-    //png_set_swap_alpha(png_ptr);
-
-    row_pointers = (png_byte **) malloc(img->height * sizeof(png_byte *));
-    for (k = 0; k < img->height; k++)
-        row_pointers[k] = img->buffer + img->stride * k;
-
-    png_write_image(png_ptr, row_pointers);
-    png_write_end(png_ptr, info_ptr);
+  if (setjmp(png_jmpbuf(png_ptr))) {
     png_destroy_write_struct(&png_ptr, &info_ptr);
-
-    free(row_pointers);
-
     fclose(fp);
+    return;
+  }
+
+  fp = fopen(fname, "wb");
+  if (fp == NULL) {
+    printf("PNG Error opening %s for writing!\n", fname);
+    return;
+  }
+
+  png_init_io(png_ptr, fp);
+  png_set_compression_level(png_ptr, 0);
+
+  png_set_IHDR(png_ptr, info_ptr, img->width, img->height, 8,
+               PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
+               // 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+               PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+  png_write_info(png_ptr, info_ptr);
+
+  png_set_bgr(png_ptr);
+  // png_set_swap_alpha(png_ptr);
+
+  row_pointers = (png_byte **)malloc(img->height * sizeof(png_byte *));
+  for (k = 0; k < img->height; k++)
+    row_pointers[k] = img->buffer + img->stride * k;
+
+  png_write_image(png_ptr, row_pointers);
+  png_write_end(png_ptr, info_ptr);
+  png_destroy_write_struct(&png_ptr, &info_ptr);
+
+  free(row_pointers);
+
+  fclose(fp);
 }
 
-static void init(int frame_w, int frame_h)
-{
-    ass_library = ass_library_init();
-    if (!ass_library) {
-        printf("ass_library_init failed!\n");
-        exit(1);
-    }
+static void init(int frame_w, int frame_h) {
+  ass_library = ass_library_init();
+  if (!ass_library) {
+    printf("ass_library_init failed!\n");
+    exit(1);
+  }
 
-    ass_set_message_cb(ass_library, msg_callback, NULL);
+  ass_set_message_cb(ass_library, msg_callback, NULL);
 
-    ass_renderer = ass_renderer_init(ass_library);
-    if (!ass_renderer) {
-        printf("ass_renderer_init failed!\n");
-        exit(1);
-    }
+  ass_renderer = ass_renderer_init(ass_library);
+  if (!ass_renderer) {
+    printf("ass_renderer_init failed!\n");
+    exit(1);
+  }
 
-    ass_set_frame_size(ass_renderer, frame_w, frame_h);
-    ass_set_fonts(ass_renderer, NULL,"Sans", 1, NULL, 1);
-    
-    if (0) {
-      ass_set_fonts(
-        ass_renderer,
-        "/usr/share/fonts/truetype/wqy/wqy-microhei.ttc", //NULL,
-        "Sans",
-        1,
-        NULL,
-        1
-      );
-    }
+  ass_set_frame_size(ass_renderer, frame_w, frame_h);
+  ass_set_fonts(ass_renderer, NULL, "Sans", 1, NULL, 1);
+
+  if (0) {
+    ass_set_fonts(ass_renderer,
+                  "/usr/share/fonts/truetype/wqy/wqy-microhei.ttc",  // NULL,
+                  "Sans", 1, NULL, 1);
+  }
 }
 
-static image_t *gen_image(int width, int height)
-{
-    image_t *img = malloc(sizeof(image_t));
-    img->width = width;
-    img->height = height;
-    img->stride = width * 4;
-    img->buffer = (unsigned char *) calloc(1, height * width * 4);
+static image_t *gen_image(int width, int height) {
+  image_t *img = malloc(sizeof(image_t));
+  img->width = width;
+  img->height = height;
+  img->stride = width * 4;
+  img->buffer = (unsigned char *)calloc(1, height * width * 4);
 
-    memset(img->buffer, 0, (unsigned long)(img->stride * img->height));
-    int i;
-        for (i = 0; i < height * width * 4; ++i) {
-            if (i%4 == 3) {
-                img->buffer[i] = 0;
-            }
-        // img->buffer[i] = (i/3/50) % 100;
-        }
-    return img;
-}
-
-#define _r(c)  ((c)>>24)
-#define _g(c)  (((c)>>16)&0xFF)
-#define _b(c)  (((c)>>8)&0xFF)
-#define _a(c)  ((c)&0xFF)
-
-static void blend_single(image_t * frame, ASS_Image *img)
-{
-    int x, y;
-    unsigned char opacity = 255 - _a(img->color);
-    unsigned char r = _r(img->color);
-    unsigned char g = _g(img->color);
-    unsigned char b = _b(img->color);
-
-    unsigned char *src;
-    unsigned char *dst;
-        int d = 4;
-    src = img->bitmap;
-    dst = frame->buffer + img->dst_y * frame->stride + img->dst_x * d;
-    for (y = 0; y < img->h; ++y) {
-        for (x = 0; x < img->w; ++x) {
-            unsigned k = ((unsigned) src[x]) * opacity / 255;
-            // possible endianness problems
-            dst[x * d + 0] = (k * b + (255 - k) * dst[x * d + 0]) / 255;
-            dst[x * d + 1] = (k * g + (255 - k) * dst[x * d + 1]) / 255;
-            dst[x * d + 2] = (k * r + (255 - k) * dst[x * d + 2]) / 255;
-            if (k) {
-                dst[x*d+3] = 255;
-            }
-        }
-        src += img->stride;
-        dst += frame->stride;
+  memset(img->buffer, 0, (unsigned long)(img->stride * img->height));
+  int i;
+  for (i = 0; i < height * width * 4; ++i) {
+    if (i % 4 == 3) {
+      img->buffer[i] = 0;
     }
+    // img->buffer[i] = (i/3/50) % 100;
+  }
+  return img;
 }
 
-static void blend(image_t * frame, ASS_Image *img)
-{
-    int cnt = 0;
-    while (img) {
-        blend_single(frame, img);
-        ++cnt;
-        img = img->next;
+#define _r(c) ((c) >> 24)
+#define _g(c) (((c) >> 16) & 0xFF)
+#define _b(c) (((c) >> 8) & 0xFF)
+#define _a(c) ((c)&0xFF)
+
+static void blend_single(image_t *frame, ASS_Image *img) {
+  int x, y;
+  unsigned char opacity = 255 - _a(img->color);
+  unsigned char r = _r(img->color);
+  unsigned char g = _g(img->color);
+  unsigned char b = _b(img->color);
+
+  unsigned char *src;
+  unsigned char *dst;
+  int d = 4;
+  src = img->bitmap;
+  dst = frame->buffer + img->dst_y * frame->stride + img->dst_x * d;
+  for (y = 0; y < img->h; ++y) {
+    for (x = 0; x < img->w; ++x) {
+      unsigned k = ((unsigned)src[x]) * opacity / 255;
+      // possible endianness problems
+      dst[x * d + 0] = (k * b + (255 - k) * dst[x * d + 0]) / 255;
+      dst[x * d + 1] = (k * g + (255 - k) * dst[x * d + 1]) / 255;
+      dst[x * d + 2] = (k * r + (255 - k) * dst[x * d + 2]) / 255;
+      if (k) {
+        dst[x * d + 3] = 255;
+      }
     }
-    printf("%d images blended\n", cnt);
+    src += img->stride;
+    dst += frame->stride;
+  }
 }
 
-int main(int argc, char *argv[])
-{
+static void blend(image_t *frame, ASS_Image *img) {
+  int cnt = 0;
+  while (img) {
+    blend_single(frame, img);
+    ++cnt;
+    img = img->next;
+  }
+  printf("%d images blended\n", cnt);
+}
+
+int main(int argc, char *argv[]) {
   printf("%s %s\n", argv[3], argv[4]);
 
-    if (argc < 6) {
-        printf("usage: %s <image file> <subtitle file> <time> <width> <height>\n", argv[0]);
-        exit(1);
-    }
+  if (argc < 6) {
+    printf("usage: %s <image file> <subtitle file> <time> <width> <height>\n",
+           argv[0]);
+    exit(1);
+  }
 
-    const int frame_w = atoi(argv[4]);
-    const int frame_h = atoi(argv[5]);
+  const int frame_w = atoi(argv[4]);
+  const int frame_h = atoi(argv[5]);
 
-    char *imgfile = argv[1];
-    char *subfile = argv[2];
-    double tm = strtod(argv[3], 0);
+  char *imgfile = argv[1];
+  char *subfile = argv[2];
+  double tm = strtod(argv[3], 0);
 
-    init(frame_w, frame_h);
-    ASS_Track *track = ass_read_file(ass_library, subfile, NULL);
-    if (!track) {
-        printf("track init failed!\n");
-        return 1;
-    }
+  init(frame_w, frame_h);
+  ASS_Track *track = ass_read_file(ass_library, subfile, NULL);
+  if (!track) {
+    printf("track init failed!\n");
+    return 1;
+  }
 
-    ASS_Image *img =
-        ass_render_frame(ass_renderer, track, (int) (tm * 1000), NULL);
-    image_t *frame = gen_image(frame_w, frame_h);
-    blend(frame, img);
+  ASS_Image *img =
+      ass_render_frame(ass_renderer, track, (int)(tm * 1000), NULL);
+  image_t *frame = gen_image(frame_w, frame_h);
+  blend(frame, img);
 
-    ass_free_track(track);
-    ass_renderer_done(ass_renderer);
-    ass_library_done(ass_library);
+  ass_free_track(track);
+  ass_renderer_done(ass_renderer);
+  ass_library_done(ass_library);
 
-    write_png(imgfile, frame);
-    free(frame->buffer);
-    free(frame);
+  write_png(imgfile, frame);
+  free(frame->buffer);
+  free(frame);
 
-    return 0;
+  return 0;
 }
